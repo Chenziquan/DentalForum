5a755bdfe34c19394e56a7735ec3d2d7176528be
Chenziquan <chenjunquan0208@gmail.com>
2016-12-06 18:17:19 +0800

    12.6提交

--- CurriculumDesign/dentalforum/src/main/java/jqchen/dentalforum/recommend/RecommendFragment.java
+++ CurriculumDesign/dentalforum/src/main/java/jqchen/dentalforum/recommend/RecommendFragment.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewStub;
 
@@ -63,6 +64,7 @@ public class RecommendFragment extends BaseFragment implements RecommendContract
         recommendRecycler.setNestedScrollingEnabled(true);
         recommendRecycler.setLayoutManager(new LinearLayoutManager(this.getContext()));
         recommendAdapter = new RecommendAdapter(R.layout.item_recommend, data);
+        recommendAdapter.addHeaderView(LayoutInflater.from(this.getContext()).inflate(R.layout.network_error,null));
         recommendRecycler.setAdapter(recommendAdapter);
     }
 
